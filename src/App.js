96f8d915e34caba94639e80e1e255f42ca7e6874
Kai cheng Wu <kaichengwu@Kais-MacBook-Pro.local>
2022-06-20 00:15:28 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Start developing!</h1>
+      <button className='btn btn-success me-2'>
+        <i className ="fa fa-home"/> Yooo </button>
+      <button className='btn btn-success me-2'> Yooo </button>
+    </div>
+  );
+}
+
+export default App;
