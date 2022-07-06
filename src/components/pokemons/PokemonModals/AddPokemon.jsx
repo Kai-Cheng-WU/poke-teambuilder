cd8afee40c04cefaee19cc2e47681953de326e6c
Kai-Cheng-Wu <kaicheng.wu@mail.mcgill.ca>
2022-07-06 19:50:38 -0400

    .

--- src/components/pokemons/PokemonModals/AddPokemon.jsx
+++ src/components/pokemons/PokemonModals/AddPokemon.jsx
@@ -80,22 +80,53 @@ let AddPokemon = ({open, onClose}) => {
                             <form style={{maxWidth: "100%"}}>
                                 <h1>{pokemon.name}</h1>
                                 <div className='mb-2'>
-                                    <h3><i class="fa-solid fa-heart"></i>: {pokemon.base_hp} + <input className='evField' type="number" placeholder='hp EV'/> + <input className='ivField' type="number" placeholder='hp IV'/> =  </h3>
+                                    <h3><i class="fa-solid fa-heart"></i>: {pokemon.base_hp} + <input className='evField' type="number" placeholder='hp EV' min="0" max="252" step="4"/> + <input className='ivField' type="number" placeholder='hp IV' min="0" max="31"/> =  </h3>
                                 </div>
                                 <div className='mb-2'>
-                                    <h3><i class="fa-solid fa-hand-fist"></i>: {pokemon.base_atk} + <input className='evField' type="number" placeholder='atk EV'/> + <input className='ivField' type="number" placeholder='atk IV'/> =  </h3>
+                                    <h3><i class="fa-solid fa-hand-fist"></i>: {pokemon.base_atk} + <input className='evField' type="number" placeholder='atk EV' min="0" max="252" step="4"/> + <input className='ivField' type="number" placeholder='atk IV' min="0" max="31"/> =  </h3>
                                 </div>
                                 <div className='mb-2'>
-                                    <h3><i class="fa-solid fa-shield-halved"></i>: {pokemon.base_def} + <input className='evField' type="number" placeholder='def EV'/> + <input className='ivField' type="number" placeholder='def IV'/> =  </h3>
+                                    <h3><i class="fa-solid fa-shield-halved"></i>: {pokemon.base_def} + <input className='evField' type="number" placeholder='def EV' min="0" max="252" step="4"/> + <input className='ivField' type="number" placeholder='def IV' min="0" max="31"/> =  </h3>
                                 </div>
                                 <div className='mb-2'>
-                                    <h3><i class="fa-solid fa-wand-sparkles"></i>: {pokemon.base_spa} + <input className='evField' type="number" placeholder='spa EV'/> + <input className='ivField' type="number" placeholder='spa IV'/> =  </h3>
+                                    <h3><i class="fa-solid fa-wand-sparkles"></i>: {pokemon.base_spa} + <input className='evField' type="number" placeholder='spa EV' min="0" max="252" step="4"/> + <input className='ivField' type="number" placeholder='spa IV' min="0" max="31"/> =  </h3>
                                 </div>
                                 <div className='mb-2'>
-                                    <h3><i class="fa-solid fa-circle-half-stroke"></i>: {pokemon.base_spd} + <input className='evField' type="number" placeholder='spd EV'/> + <input className='ivField' type="number" placeholder='spd IV'/> =  </h3>
+                                    <h3><i class="fa-solid fa-circle-half-stroke"></i>: {pokemon.base_spd} + <input className='evField' type="number" placeholder='spd EV' min="0" max="252" step="4"/> + <input className='ivField' type="number" placeholder='spd IV' min="0" max="31"/> =  </h3>
                                 </div>
                                 <div className='mb-2'>
-                                    <h3><i class="fa-solid fa-person-running"></i>: {pokemon.base_spe} + <input className='evField' type="number" placeholder='spe EV'/> + <input className='ivField' type="number" placeholder='spe IV'/> =  </h3>
+                                    <h3><i class="fa-solid fa-person-running"></i>: {pokemon.base_spe} + <input className='evField' type="number" placeholder='spe EV' min="0" max="252" step="4"/> + <input className='ivField' type="number" placeholder='spe IV' min="0" max="31"/> =  </h3>
+                                </div>
+                                <div className='mb-2'>
+                                    <h3><i class="fa-solid fa-face-grin-wide"></i>: &nbsp;&nbsp;&nbsp;&nbsp;
+                                        
+                                            <select>
+                                                <option value="Hardy">Hardy</option>
+                                                <option value="Lonely">Lonely</option>
+                                                <option value="Brave">Brave</option>
+                                                <option value="Adamant">Adamant</option>
+                                                <option value="Naughty">Naughty</option>
+                                                <option value="Bold">Bold</option>
+                                                <option value="Relaxed">Relaxed</option>
+                                                <option value="Impish">Impish</option>
+                                                <option value="Lax">Lax</option>
+                                                <option value="Timid">Timid</option>
+                                                <option value="Hasty">Hasty</option>
+                                                <option value="Serious">Serious</option>
+                                                <option value="Jolly">Jolly</option>
+                                                <option value="Naive">Naive</option>
+                                                <option value="Modest">Modest</option>
+                                                <option value="Mild">Mild</option>
+                                                <option value="Quiet">Quiet</option>
+                                                <option value="Bashful">Bashful</option>
+                                                <option value="Rash">Rash</option>
+                                                <option value="Calm">Calm</option>
+                                                <option value="Gentle">Gentle</option>
+                                                <option value="Sassy">Sassy</option>
+                                                <option value="Careful">Careful</option>
+                                                <option value="Quirky">Quirky</option>
+                                            </select>
+                                    </h3>
                                 </div>
                                 
  
