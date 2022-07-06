ffcf75d3826d44e5f3836dc8740b531cbd064ffe
Kai-Cheng-Wu <kaicheng.wu@mail.mcgill.ca>
2022-07-05 21:53:16 -0400

    WORKING...

--- src/components/pokemons/PokemonModals/AddPokemon.jsx
+++ src/components/pokemons/PokemonModals/AddPokemon.jsx
@@ -9,6 +9,12 @@ let AddPokemon = ({open, onClose}) => {
         name: "",
         species: "",
         img: "",
+        base_hp: "",
+        base_atk: "",
+        base_def: "",
+        base_spa: "",
+        base_spd: "",
+        base_spe: "",
     });
 
     const searchPokemon = (event) => {
@@ -18,7 +24,14 @@ let AddPokemon = ({open, onClose}) => {
             setPokemon({
                 name: pokemonName, 
                 species: response.data.species.namem, 
-                img: response.data.sprites.front_default});
+                img: response.data.sprites.front_default,
+                base_hp: response.data.stats[0].base_stat,
+                base_atk: response.data.stats[1].base_stat,
+                base_def: response.data.stats[2].base_stat,
+                base_spa: response.data.stats[3].base_stat,
+                base_spd: response.data.stats[4].base_stat,
+                base_spe: response.data.stats[5].base_stat,
+            });
         });
         setPokemonChosen(true)
     };
@@ -64,8 +77,28 @@ let AddPokemon = ({open, onClose}) => {
                         </div>
                         
                         <div className='col-md-4'>
-                            <form>
-
+                            <form style={{maxWidth: "100%"}}>
+                                <h1>{pokemon.name}</h1>
+                                <div className='mb-2'>
+                                    <h3>Health: {pokemon.base_hp} + <input className='evField' type="text" placeholder='hp EV'/> + <input className='ivField' type="text" placeholder='hp IV'/> =  </h3>
+                                </div>
+                                <div className='mb-2'>
+                                    <h3>Attack: {pokemon.base_atk} + <input className='evField' type="text" placeholder='atk EV'/> + <input className='ivField' type="text" placeholder='atk IV'/> =  </h3>
+                                </div>
+                                <div className='mb-2'>
+                                    <h3>Defense: {pokemon.base_def} + <input className='evField' type="text" placeholder='def EV'/> + <input className='ivField' type="text" placeholder='def IV'/> =  </h3>
+                                </div>
+                                <div className='mb-2'>
+                                    <h3>Special Attack: {pokemon.base_spa} + <input className='evField' type="text" placeholder='spa EV'/> + <input className='ivField' type="text" placeholder='spa IV'/> =  </h3>
+                                </div>
+                                <div className='mb-2'>
+                                    <h3>Special Defense: {pokemon.base_spd} + <input className='evField' type="text" placeholder='spd EV'/> + <input className='ivField' type="text" placeholder='spd IV'/> =  </h3>
+                                </div>
+                                <div className='mb-2'>
+                                    <h3>Speed: {pokemon.base_spe} + <input className='evField' type="text" placeholder='spe EV'/> + <input className='ivField' type="text" placeholder='spe IV'/> =  </h3>
+                                </div>
+                                
+ 
                             </form>
                         </div>
                     </div>
